975fe092684900600d7ac0ba1b447dbac51bd8b4
OraveczJozsef <78857101+OraveczJozsef@users.noreply.github.com>
2021-03-01 12:01:49 +0100

    Update Main.java

--- 04_Hazi/src/Feladat/Main.java
+++ 04_Hazi/src/Feladat/Main.java
@@ -16,8 +16,9 @@ public class Main {
 			
 			try {
 				szemely = new Szemely(tempArray[0], Integer.parseInt(tempArray[1]), Double.parseDouble(tempArray[2]));
+				isValid = true;
 				/*
-					!!! Konstruktor nélkül !!!
+					!!! Konstruktor nÃ©lkÃ¼l !!!
 					
 					szemely = new Szemely();
 					szemely.setNev(tempArray[0]);
